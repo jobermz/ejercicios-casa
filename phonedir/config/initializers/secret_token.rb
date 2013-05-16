f71ea1aa993442460449febc0efd3dabc0ebeea8
Jober Mena <jober.mena@gmail.com>
2013-05-15 20:29:57 -0500

    Primer programa con Ruby on Rails

---
+++ phonedir/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phonedir::Application.config.secret_token = 'bca2bc0c6c98256be6340ae8a7a42af264e2f8ec4a752d41573c4901a7c908cdea5cf954b8fb36807ebea9939258889bf54298e179d0f924d6528efb88f13da2'
